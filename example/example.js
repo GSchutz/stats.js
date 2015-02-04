3a8bf4e4d89d6d9514cc25c1a67493aa987e7794
gschutz <guilherme@gschutz.com>
2015-02-04 20:56:30 -0200

    Added the jsdoc package for the documentation; added some documentation and create some examples; grouped some methods in one baseOperator; Added the round method with precision; create a copy method to return new StatsWrapper object and not overwrite; create a aggregate/dispatch method to group functions that can be calculated in the same loop;

--- example/example.js
+++ example/example.js
@@ -1,50 +1,233 @@
 /**
  * Example.js is a demonstration of the Stats.js
  */
+var __perf_counter = 1;
+var __eg_counter = 1;
 
-_.mixin(_.pick(st, ['subtract', 'divide', 'multiply', 'sum']));
+/*------------- //////////////////// -------------*/
+/*------------- // START Examples // -------------*/
+/*------------- //////////////////// -------------*/
 
-console.log(_.sum(1,2));
+function eg(method) {
+	console.log('------- '+ __eg_counter +') Example '+ method +' -------')
+	return __eg_counter++;
+}
 
-var __perf_counter = 0;
-	
-console.log(' ------ test 1 -------')
+//////////////////
+// Example: sum //
+//////////////////
+eg('sum');
 
-var calc = st(1)
-	.sum(3)
-	.divide(2)
-	.sum(4)
-	.multiply(2)
-	.subtract(3)
-	.sum(1)
-	.multiply(3);
+var data = [5,7,1,4];
+
+// sum all data values
+var calc = st(data).sum();
 
 console.log(calc.value());
+// => 17
 
-console.log(' ------ test 2 -------')
+// sum all data values
+var calc = st(data).sum([2,3,1,2]);
+
+console.log(calc.value());
+// => 17
+
+// sum data values by values
+var values = [1,0,2,-2];
+
+var calc = st(data).sum(values);
+
+console.log(calc.value());
+// => [6, 7, 3, 2]
+
+
+///////////////////////
+// Example: subtract //
+///////////////////////
+eg('subtract');
+
+var data = [5,7,1,4];
+
+// subtract '1' from all data values
+var calc = st(data).subtract(1);
+
+console.log(calc.value());
+// =>  [4, 6, 0, 3]
+
+// subtract each data value by each values
+var values = [1,0,2,-2];
+
+var calc = st(data).subtract(values);
+
+console.log(calc.value(), data);
+// => [3, 6, -2, 5]
+
+
+/////////////////////
+// Example: divide //
+/////////////////////
+eg('divide');
+
+var data = [5, 7, 1, 4];
+
+// divide '2' from all data values
+var calc = st(data).divide(2);
+
+console.log(calc.value());
+// =>  [2.5, 3.5, 0.5, 2]
+
+// divide each data value by each values
+var values = [1, 7, 0.5, -2];
+
+var calc = st(data).divide(values);
+
+console.log(calc.value(), data);
+// => [5, 1, 2, -2]
+
+
+/////////////////////
+// Example: multiply //
+/////////////////////
+eg('multiply');
+
+var data = [-3, 1, 0, 0.5, 17];
+
+// multiply all data values by '2'
+var calc = st(data).multiply(2);
+
+console.log(calc.value());
+// =>  [-6, 2, 0, 1, 34]
+
+// multiply each data value by each values
+var values = [3, 7, -2, 0.5, -1];
+
+var calc = st(data).multiply(values);
+
+console.log(calc.value());
+// => [-9, 7, -0, 0.25, -17]
+
+
+////////////////////////
+// Example: aggregate //
+////////////////////////
+eg('aggregate');
+
+var data = [1,2,3,4];
+
+var calc = st(data)
+	// start aggregate
+	.aggregate()
+		.sum([2,-6,2,2])
+		.sum(-2)
+	// stop aggregate
+	.dispatch();
+
+console.log(calc.value());
+// => [1, -6, 3, 4]
+
+
+////////////////////////
+// Example: aggregate //
+////////////////////////
+eg('copy');
+
+var price = st([10, 30, 80]);
+
+var newPrice = price.multiply(1.1);
+
+console.log(price.value());
+// => [11, 33, 88]
+
+console.log(newPrice.value());
+// => [11, 33, 88]
+
+// The price has been changed, 
+// because we create one wrapper.
+// If this is a problem, just make a copy:
+
+var price = st([10, 30, 80]);
+
+var newPrice = price.copy().multiply(1.1);
+
+console.log(price.value());
+// => [10, 30, 80]
+
+console.log(newPrice.value());
+// => [11, 33, 88]
+
+
+
+////////////////////
+// Example: round //
+////////////////////
+eg('round');
+
+var price = st([10.876, 11.001, 55.000000001]).round(10);
+
+console.log(price);
+// => [2, 4, 6, 8]
+
+
+/*------------- ////////////////// -------------*/
+/*------------- // END Examples // -------------*/
+/*------------- ////////////////// -------------*/
+	
+
+function sum() {
+	var calc = st([1,4])
+		.sum([3,2,5])
+		.sum(4);
+
+	return calc.value();
+}
+
+perf(sum);
+	
+
+function divide() {
+	var calc = st([1,2,2])
+		.divide(2)
+		.divide([2,2]);
+
+
+	return calc.value();
+}
+
+perf(divide);
+
+function subtract() {
+	var calc = st([13,23])
+		.subtract(3)
+		.sum([4,5])
+		.subtract([0,5])
+		.sum();
+
+
+	return calc.value();
+}
+
+perf(subtract);
+
+//console.log(calc.value(), calc2.value());
+
 
 function test2() {
 	var funcs = ['divide', 'divide', 'multiply', 'sum'];
 
-	var calc = _(1);
+	var calc = st(1);
 
-	for(var i = 0; i < 10000; i++) {
-		calc = calc[_(funcs).sample()](_.random(1, 10));
+	for(var i = 0; i < 100000; i++) {
+		calc[_(funcs).sample()](_.random(1, 10));
 	}
 
-	var r = calc.take(1000).value();
+	var r = calc.value();
 
 	console.log(r);
 
 	return r;
 };
 
-//perf(function() {
-//	console.log(perf(test2).value());
-//});
-perf(test2)
-
-console.log(' ------ test 3 -------')
+//perf(test2)
 
 function test3() {
 	var funcs = ['divide', 'divide', 'multiply', 'sum'];
@@ -62,9 +245,26 @@ function test3() {
 
 //perf(test3);
 
+var soma = function(a,b) {
+	return a + b;
+}
+
+function test4()  {
+	var a = [2,34,5,-46,7,-7,-4,-3,4];
+	var r = 0;
+
+	r = st(a).sum();
+
+	console.log(a.length);
+
+	return r;
+}
+
+//console.log(perf(test4));
 
 
 function perf(fn) {
+	console.log(' ------ test '+ __perf_counter +' -------')
 	__perf_counter += 1;
 
 	var i = (new Date()).getTime();
@@ -72,6 +272,8 @@ function perf(fn) {
 	var v = fn();
 
 	var o = (new Date()).getTime();
+
+	console.log(v);
 
 	console.log(o - i);
 
--- stats.js
+++ stats.js
@@ -9,76 +9,493 @@
  * gschutz.com
  */
 
-
 ;(function(exports) {
+
+	/**
+	 * @namespace constants
+	 */
+
+	/**
+	 * Default precision for all calculations. Defining this limit ensure the floating point problem.
+	 *
+	 * @memberOf constants
+	 *
+	 * @type {Number}
+	 */
+	var PRECISION = 10;
+
+
 	// chain is a object/array/string/number, any value that can be manipulated by Stats
 	
-	function Stats(value, chainAll, actions) {
-		this.__actions__ = actions || [];
+	function StatsWrapper(value, chainAll, actions) {
+		this.__aggregate__ = false;
 		this.__chain__ = !!chainAll;
-		this.__wrapped__ = value;
+		this.__wrapped__ = _.clone(value);
 	}
 
-
+	/**
+	 * The object creator itself.
+	 *
+	 * @method   stats
+	 *
+	 * @category
+	 *
+	 * @param    {*} wrapper Any value to be wrapped for chaining methods.
+	 *
+	 * @return   {StatsWrapper}         Always return a `StatsWrapper`.
+	 *
+	 * @namespace stats
+	 */
 	function stats(wrapper){
-		if (_.isPlainObject(wrapper) && !_.isArray(wrapper)) {
+		if (_.isObject(wrapper) && !_.isArray(wrapper)) {
 			if (wrapper instanceof StatsWrapper) {
 				return wrapper;
 			}
 			if (_.hasOwnProperty.call(wrapper, '__wrapped__')) {
-				return new Stats(wrapper.__wrapped__, wrapper.__chain__, _.clone(wrapper.__actions__));
+				return new StatsWrapper((wrapper.__wrapped__), wrapper.__chain__, wrapper.__actions__);
 			}
 		}
-		return new Stats(wrapper);
+		return new StatsWrapper((wrapper));
 	}
 
+	/**
+	 * Transform all parameters into a unique `array`, even multidimensional
+	 * arrays and objects.
+	 *
+	 * @method   linearize
+	 *
+	 * @static
+	 *
+	 * @memberOf stats
+	 *
+	 * @category math
+	 *
+	 * @param {...*} [*] Any value.
+	 *
+	 * @return {Array} Returns an `array` with all parameters linearized.
+	 */
+	function linearize() {
+		var args = _.toArray(arguments),
+			r = [];
+		
+		_.each(args, function(t){
+			if (_.isArray(t) || _.isPlainObject(t)) {
+				_.each(t, function(d){
+						if (_.isArray(d) || _.isPlainObject(d)) {
+							r = r.concat(linearize(d));
+						} else {
+							r.push(d);
+						}
+				});
+			} else {
+				r.push(t);
+			}
+		});
+		
+		return r;
+	}
 
+	function baseOperator(a, b, fn) {
+		if (_.isFinite(a) && _.isFinite(b)) {
+			return fn(a, b);
+		}
+
+		if (_.isArray(a) && !b) {
+			var r = 0;
+			a = linearize(a);
+
+    	_.each(a, function(c, i){
+    		if (_.isFinite(c))
+    			r = fn(r, c);
+  		});
+  		return r;
+		}
+
+		if (_.isArray(a) && _.isFinite(b)) {
+			a = linearize(a);
+
+    	_.each(a, function(c, i){
+    		if (_.isFinite(c))
+    			a[i] = fn(a[i], b);
+  		});
+		}
+
+		if (_.isArray(a) && _.isArray(b)) {
+			a = linearize(a);
+			b = linearize(b);
+
+    	_.each(a, function(c, i){
+    		if (_.isFinite(c) && _.isFinite(b[i]))
+    			a[i] = fn(a[i], b[i]);
+  		});
+		}
+
+		if (_.isFinite(a) && _.isArray(b)) {
+			b = linearize(b);
+
+    	_.each(b, function(c, i){
+    		if (_.isFinite(c))
+    			a = fn(a, c);
+  		});
+		}
+    
+    return a;
+	}
+
+	/**
+	 * Sum all values passed in argument `a` by argument `b`, accept almost any
+	 * number inside any collection (array or plain object)
+	 *
+	 * @method   sum
+	 *
+	 * @static
+	 *
+	 * @memberOf stats
+	 *
+	 * @category math
+	 *
+	 * @param {(number|array)} a The `number` or `array` to add from.
+	 * @param {(number|array)} b The `number` or `array` that will be added.
+	 *
+	 * @return {(number|array)}   Always return the same type passed by `a`.
+	 *
+	 * @example
+	 *
+	 * var data = [5,7,1,4];
+	 *
+	 * // sum all data values 
+	 * 
+	 * var calc = st(data).sum();
+	 *
+	 * console.log(calc.value()); 
+	 * // => 17
+	 *
+	 * // sum data values by values 
+	 * 
+	 * var values = [1,0,2,-2];
+	 *
+	 * var calc = st(data).sum(values);
+	 *
+	 * console.log(calc.value()); 
+	 * // => [6, 7, 3, 2] 
+	 */
 	function sum(a, b) {
-		return a + b;
+		return baseOperator(a, b, function(a, b) {
+			return a + b;
+		});
 	}
 
+	/**
+	 * Subtracts `a` by `b` (`a - b`). If `b` and `a` are a list subtracts each
+	 * element in list `b` from each element in list `a`.
+	 *
+	 * @method   subtract
+	 *
+	 * @static
+	 *
+	 * @memberOf stats
+	 *
+	 * @category math
+	 *
+	 * @param {(number|array)} a The `number` or `array` to subtracts from.
+	 * @param {(number|array)} b The `number` or `array` that will subtracts a.
+	 *
+	 * @return {(number|array)} Always return the same type passed by `a`.
+	 */
 	function subtract(a, b) {
-		return a - b;
+		return baseOperator(a, b, function(a, b) {
+			return a - b;
+		});
 	}
 
+	/**
+	 * Divide each `a` by `b` (`a / b`). If `a` is a list, divide each element
+	 * in `a` by `b` (or each element in `b`). If `a` is a `number` and `b` a
+	 * list, divide `a` by every element in `b`.
+	 *
+	 * @method   divide
+	 *
+	 * @memberOf stats
+	 *
+	 * @category math
+	 *
+	 * @param    {(number|array)} a The `number` or `array` that will be divided.
+	 * @param    {(number|array)} b The `number` or `array` that will divide.
+	 *
+	 * @return   {(number|array)}   Always return the same type passed by `a`.
+	 *
+	 * @example
+	 *
+	 * 
+	 * var data = [5, 7, 1, 4];
+	 * 
+	 * // divide '2' from all data values
+	 * var calc = st(data).divide(2);
+	 * 
+	 * console.log(calc.value());
+	 * // =>  [2.5, 3.5, 0.5, 2]
+	 * 
+	 * // divide each data value by each values
+	 * var values = [1, 7, 0.5, -2];
+	 * 
+	 * var calc = st(data).divide(values);
+	 * 
+	 * console.log(calc.value(), data);
+	 * // => [5, 1, 2, -2]
+	 * 
+	 */
 	function divide(a, b) {
-		return a / b;
+		return baseOperator(a, b, function(a, b) {
+			return a / b;
+		});
 	}
 
+	/**
+	 * Take element `a` (or each element in `a`), and multiply by `b` (or each
+	 * element in `b`).
+	 *
+	 * @method   multiply
+	 *
+	 * @memberOf stats
+	 *
+	 * @category math
+	 *
+	 * @param    {(number|array)} a The `number` or `array` that will be multiplied.
+	 * @param    {(number|array)} b The `number` or `array` that will multiply.
+	 *
+	 * @return   {(number|array)}   Always return the same type passed by `a`.
+	 *
+	 * @example
+	 *
+	 * 
+	 * var data = [-3, 1, 0, 0.5, 17];
+	 * 
+	 * // multiply all data values by '2'
+	 * var calc = st(data).multiply(2);
+	 * 
+	 * console.log(calc.value());
+	 * // =>  [-6, 2, 0, 1, 34]
+	 * 
+	 * // multiply each data value by each values
+	 * var values = [3, 7, -2, 0.5, -1];
+	 * 
+	 * var calc = st(data).multiply(values);
+	 * 
+	 * console.log(calc.value());
+	 * // => [-9, 7, -0, 0.25, -17]
+	 * 
+	 */
 	function multiply(a, b) {
-		return a * b;
+		return baseOperator(a, b, function(a, b) {
+			return a * b;
+		});
 	}
 
+	/**
+	 * Just round the variable passed to the passed precision (default is 10 decimals).
+	 *
+	 * @method   round
+	 *
+	 * @memberOf stats
+	 *
+	 * @category math
+	 *
+	 * @param    {(number|array)} value       Any `integer` or `float` number, or list of `number`.
+	 * @param    {number} precision A non-negative `integer`.
+	 *
+	 * @return   {(number|array)}           Return the same type of `value`.
+	 */
+	function round(value, precision) {
+		if (_.isFinite(value)) {
+			return baseRound(value, precision);
+		}
+
+		if (_.isObject(value)) {
+			value = linearize(value);
+
+			_.each(value, function(c, i) {
+				value[i] = baseRound(c, precision);
+			}, this);
+		}
+
+		return value;
+	}
+
+	function baseRound(num, precision) {
+		// for maximum accuracy, the parameter number for toFixed should be bigger.
+		// i.e., for complex calculus, with bigger numbers, this entire function
+		// needs to be revised.
+
+		//  This is a workaround for round up with no decimals.
+		var rounded, original_num = num;
+
+		num = parseFloat(num);
+
+		if( precision === 0 )
+			num = roundTo(num/10, 1) * 10;
+
+		precision = precision || PRECISION;
+		rounded = +(Math.round(num.toFixed(8) + "e+" + precision)  + "e-" + precision);
+
+		// this error never happen, remeber to remove this code on production.
+		if (_.isNaN(rounded) || !_.isNumber(rounded)) {
+			var caller_line = (new Error).stack;
+			console.error(rounded, original_num);
+			console.error(caller_line);
+		}
+
+		return rounded;
+	}
+
+	/**
+	 * Return the value stored (and already calculated) in the pipeline.
+	 *
+	 * @method   value
+	 *
+	 * @memberOf stats
+	 *
+	 * @category chain
+	 *
+	 * @return   {*} Returns whatever the last function applied has returned.
+	 */
 	function value() {
-		return baseWrapperValue(this.__wrapped__, this.__actions__);
+		if (this.__aggregate__) {
+			dispatch();
+		}
+
+		return this.__wrapped__;
+	}
+
+	/**
+	 * Just set the aggregate property to `true`, so that methods can be
+	 * aggregated. This method is usefull when dealing with a lot of data and
+	 * performing more than one calculations. This method just join all the
+	 * methods applied to the pipeline in one big loop.
+	 *
+	 * When dealing with complex methods, this method has not been tested
+	 * properly.
+	 *
+	 * @method   aggregate
+	 *
+	 * @memberOf stats
+	 *
+	 * @category chain
+	 *
+	 * @return   {StatsWrapper} Return the current pipeline.
+	 */
+	function aggregate() {
+		this.__aggregate__ = true;
+
+		return this;
+	}
+
+	/**
+	 * Dispatch all methods chained in the pipeline in one big loop.
+	 *
+	 * @method   dispatch
+	 *
+	 * @memberOf stats
+	 *
+	 * @category chain
+	 *
+	 * @return   {StatsWrapper} Return the current pipeline with all aggregated method results.
+	 */
+	function dispatch() {
+		// check if we are in an aggregate environment
+		if (this.__aggregate__) {
+
+			var ag = this.__aggregate__;
+
+			var r = this.value();
+
+			_.each(r, function(value, i) {
+				_.each(this.__aggregate__, function(aggregate) {
+					var b = aggregate.args[0]; 
+
+					if (_.isArray(b))
+						b = b[i];
+
+					r[i] = aggregate.fn.call(this, r[i], b);
+				}, this);
+			}, this);
+			this.__wrapped__ = r;
+		}
+		this.__aggregate__ = false;
+
+		return this;
+	}
+
+	/**
+	 * Copy the current pipeline.
+	 *
+	 * Remember that `Stats` use a pipeline approach, so when chaining methods, 
+	 * the wrapped variable is overwritten by the returned value of the method.
+	 *
+	 * @method   copy
+	 *
+	 * @memberOf stats
+	 *
+	 * @category chain
+	 *
+	 * @return   {StatsWrapper} Returns a `new StatsWrapper`, don't overwritting the wrapped variable.
+	 *
+	 * @example
+	 *
+	 * var price = st([10, 30, 80]);
+	 * 
+	 * var newPrice = price.multiply(1.1);
+	 * 
+	 * console.log(price.value());
+	 * // => [11, 33, 88]
+	 * 
+	 * console.log(newPrice.value());
+	 * // => [11, 33, 88]
+	 * 
+	 * // The price has been changed, 
+	 * // because we are using the same wrapper.
+	 * // If this is a problem, just make a copy:
+	 * 
+	 * var price = st([10, 30, 80]);
+	 * 
+	 * var newPrice = price.copy().multiply(1.1);
+	 * 
+	 * console.log(price.value());
+	 * // => [10, 30, 80]
+	 * 
+	 * console.log(newPrice.value());
+	 * // => [11, 33, 88]
+	 * 
+	 */
+	function copy() {
+		return stats(this.__wrapped__);
 	}
 
 	function baseWrapperValue(value, actions) {
-	  var result = value;
+		var result = value;
 
-	  var index = -1,
-	      length = actions.length;
+		var index = -1,
+				length = actions.length;
 
-	  while (++index < length) {
-	    var args = [result],
-	        action = actions[index];
+		while (++index < length) {
+			var args = [result],
+					action = actions[index];
 
-	    Array.prototype.push.apply(args, action.args);
-	    result = action.func.apply(action.thisArg, args);
-	  }
+			Array.prototype.push.apply(args, action.args);
+			result = action.func.apply(action.thisArg, args);
+		}
 
-	  return result;
+		return result;
 	}
 
 
 	function mixin(object, source, options) {
 		var chain = true;
 		
-    if (options === false) {
-      chain = false;
-    } else if (_.isObject(options) && 'chain' in options) {
-      chain = options.chain;
-    }
+		if (options === false) {
+			chain = false;
+		} else if (_.isObject(options) && 'chain' in options) {
+			chain = options.chain;
+		}
 		// set each method (in source) for object
 		_.each(source, function(prop, methodName) {
 			// this will set the methods to the global stats
@@ -92,16 +509,31 @@
 				return function() {
 					// __wrapped__ is just one (or) the first argument,
 					// so we pass to the first arguments;
-	        var chainAll = this.__chain__;
+					var chainAll = this.__chain__;
 
-	        if (chain || chainAll) {
-	          var result = object(this.__wrapped__);
-	          (result.__actions__ = _.clone(this.__actions__)).push({ 'func': prop, 'args': arguments, 'thisArg': object });
-	          result.__chain__ = chainAll;
-	          return result;
-	        }
-	        var args = [this.__wrapped__];
-	        Array.prototype.push.apply(args, arguments);
+					if (chain || chainAll) {
+						var args = [this.__wrapped__];
+						Array.prototype.push.apply(args, arguments);
+
+						if (this.__aggregate__) {
+							if (!_.isArray(this.__aggregate__))
+								this.__aggregate__ = [];
+
+							var aggregate = {"fn": prop, "args": arguments};
+
+							this.__aggregate__.push(aggregate);
+
+							return this;
+						}
+
+						this.__wrapped__ = prop.apply(this, args);
+						this.__chain__ = chainAll;
+
+						return this;
+					}
+
+					var args = [this.__wrapped__];
+					Array.prototype.push.apply(args, arguments);
 
 					return prop.apply(this, args);
 				};
@@ -112,6 +544,7 @@
 		return object;
 	}
 
+
 	/** First we define only the Chainable methods */
 
 	var st = {};
@@ -120,6 +553,8 @@
 	st.subtract = subtract;
 	st.divide = divide;
 	st.multiply = multiply;
+
+	st.round = round;
 
 	st.value = value;
 
@@ -130,24 +565,22 @@
 	var _st = {};
 
 	_st.value = value;
+	_st.copy = copy;
+	_st.aggregate = aggregate;
+	_st.dispatch = dispatch;
 
 	mixin.call(stats, stats, _st, false);
 
 
 	// extend the Wrapper
-	Stats.prototype = stats.prototype;
+	StatsWrapper.prototype = stats.prototype;
 
 
 
 	// Apply own methods to the stats and StatsWrapper
 
-	/*
-	_(st).each(function() {
 
-	})
-	*/
+	/*--------------------------------------------------------------------------*/
 
-  /*--------------------------------------------------------------------------*/
-
-  window.st = stats;
+	window.st = stats;
 }(window));
